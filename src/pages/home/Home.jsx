146451d6149aa974d7a79b7c7a83751e4ddfa2ea
GreatnessGrace <graceunstoppable7@gmail.com>
2023-11-02 14:52:48 +0530

    [WIP] Navbar completed for desktop

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,10 @@ import React, {useContext} from "react";
 import Layout from '../../components/layout/Layout';
 import MyContext from "../../context/data/myContext";
 function Home() {
-    const context = useContext(MyContext)
-    console.log(context)
-    // Destructure
-    const {name} = context
-    console.log(name)
+
     return (  
      <Layout>
-        <h1>Name : {name} </h1>
+        Home
      </Layout>
       )
    
