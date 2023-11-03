1845dff41a6a98a9147f672afd706bac414d034d
GreatnessGrace <graceunstoppable7@gmail.com>
2023-11-03 15:32:50 +0530

    [Completed] Filter and Search Section

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/navbar";
-import HeroSection from "../heroSection/HeroSection";
 
 function Layout({children}) {
     return (
         <div>
             <Navbar />
-            {/* <div className="content">
+            <div className="content">
                 {children}
-            </div> */}
-            <HeroSection />
+            </div>
             <Footer />
 
         </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React, {useContext} from "react";
 import Layout from '../../components/layout/Layout';
-import MyContext from "../../context/data/myContext";
+import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+
 function Home() {
 
     return (  
      <Layout>
-        Home
+        <HeroSection/>
+        <Filter/>
      </Layout>
       )
    
