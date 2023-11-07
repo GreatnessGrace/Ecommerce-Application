8b6f17a32d68d4cc907e5c5b99c5e4636bac5175
GreatnessGrace <graceunstoppable7@gmail.com>
2023-11-07 17:43:54 +0530

    [WIP] Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,55 @@ import React from "react";
 
 function Footer(params) {
     return (
-        <div>Footer</div>
+        <div>
+            <div className="bg-grey-500 m-10">
+            <div className="flex flex-row justify-around m-50">
+            <div className="">
+                    <h3>CATEGORIES</h3>
+                    <ul>
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            Order
+                        </li>
+                        <li>
+                            Local For Vocal
+                        </li>
+                        <li>
+                            Cart
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>CUSTOMER SERVICE</h3>
+                    <ul>
+                        <li>Return Policy</li>
+                        <li>About</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SERVICES</h3>
+                    <ul>
+                        <li>Privacy</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="./src/assets/react.svg" alt="Payment" />
+                </div>
+            </div>
+            </div>
+            <div className="flex flex-row justify-around"> <b>E-Unstoppable</b>
+            <p>2023 E-Unstoppable - www.eunstoppable.com</p>
+            <ul className="flex flex-row"> 
+                <li>f</li>
+                <li>T</li>
+                <li>I</li>
+                <li>L</li>
+                </ul>
+                </div>
+        </div>
     )
 }
 
