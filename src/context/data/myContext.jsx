bbb0695ee39e326035aa6a92656c2d705882f1f9
GreatnessGrace <graceunstoppable7@gmail.com>
2023-12-06 12:12:44 +0530

    [Completed] Login & Signup with Firebase

--- src/context/data/myContext.jsx
+++ src/context/data/myContext.jsx
@@ -1,4 +1,5 @@
-import { createContext  } from "react";
+import { createContext } from "react";
 
-const MyContext = createContext();
-export default MyContext;
+const myContext = createContext();
+
+export default myContext;
